994eea5935bef84cb33d996f64272ff702e7ef83
Saurabh Dutta <saurabhdutta.jk@gmail.com>
2020-08-10 23:59:59 +0100

    Use differnt interface to get Body

--- src/main/java/competitive/programming/utils/TakeProblemInput.java
+++ src/main/java/competitive/programming/utils/TakeProblemInput.java
@@ -7,9 +7,8 @@ import org.gradle.internal.impldep.com.fasterxml.jackson.databind.ObjectMapper;
 import org.takes.Request;
 import org.takes.Response;
 import org.takes.Take;
+import org.takes.rq.RqPrint;
 import org.takes.rs.RsEmpty;
-
-import java.util.Scanner;
 
 public class TakeProblemInput implements Take {
 
@@ -17,18 +16,14 @@ public class TakeProblemInput implements Take {
     private ProblemInput problemInput;
 
     @Override
-    public Response act(Request req) {
+    public Response act(final Request req) {
         System.out.println("Request Received");
         try {
+            final String body = new RqPrint(req).printBody();
             ObjectMapper mapper = new ObjectMapper();
-            Scanner scanner = new Scanner(req.body());
-            StringBuilder body = new StringBuilder();
-            while (scanner.hasNext()) {
-                body.append(scanner.next());
-            }
-            System.out.println(body.toString());
+            System.out.println(body);
             mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-            this.problemInput = mapper.readValue(body.toString(), ProblemInput.class);
+            this.problemInput = mapper.readValue(body, ProblemInput.class);
             System.out.println(mapper.writeValueAsString(this.problemInput));
         }
         catch (Exception ex) {
